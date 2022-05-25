89911ad43c9988a4a3353f5e3dd1b0317f5c8e6d
fikrimus35 <mustofa13.fikri@gmail.com>
2022-05-25 20:34:05 +0700

    Register or Sign In Functions

---
+++ signin/SignIn.go
@@ -0,0 +1,289 @@
+// Copyright (C) 2022 Lempar Proyek
+//
+// This file is part of signin.
+//
+// signin is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// signin is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with signin.  If not, see <http://www.gnu.org/licenses/>.
+
+package signin
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+
+	"cloud.google.com/go/datastore"
+	"github.com/google/uuid"
+	"google.golang.org/api/idtoken"
+	"google.golang.org/api/iterator"
+)
+
+// secretmanager "cloud.google.com/go/secretmanager/apiv1"
+// secretmanagerpb "google.golang.org/genproto/googleapis/cloud/secretmanager/v1"
+
+var projectID string
+var datastoreClient *datastore.Client
+
+type User struct {
+	Id       *datastore.Key `datastore:"__key__" json:"id"`
+	Name     string         `json:"name"`
+	Username string         `json:"username"`
+	Email    string         `json:"email"`
+	GoogleId string         `json:"-"`
+	Picture  string         `json:"picture"`
+}
+
+type UserDto struct {
+	Name     string `json:"name"`
+	Username string `json:"username"`
+	Picture  string `json:"picture"`
+}
+
+var errNotFound = errors.New("data not found in database")
+var errDuplicateUsername = errors.New("username has been picked by another account")
+var errDuplicateEmail = errors.New("email has been picked by another account")
+var errDuplicateGoogleId = errors.New("google account has been registered by another account")
+
+func (u *User) GetByGID(ctx *context.Context, gid string) error {
+	query := datastore.NewQuery("User").Filter(
+		"GoogleId =",
+		gid,
+	).Limit(1)
+	it := datastoreClient.Run(*ctx, query)
+
+	if _, err := it.Next(u); err == iterator.Done {
+		return errNotFound
+	} else if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (u *User) Create(ctx *context.Context) error {
+	tx, err := datastoreClient.NewTransaction(*ctx, datastore.ReadOnly)
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+
+	var users []User
+
+	usernameQuery := datastore.NewQuery("User").Filter("Username =", u.Username).Transaction(tx)
+	_, err = datastoreClient.GetAll(*ctx, usernameQuery, &users)
+	if err != nil {
+		return err
+	}
+	if len(users) > 0 {
+		return errDuplicateUsername
+	}
+
+	emailQuery := datastore.NewQuery("User").Filter("Email =", u.Email).Transaction(tx)
+	_, err = datastoreClient.GetAll(*ctx, emailQuery, &users)
+	if err != nil {
+		return err
+	}
+	if len(users) > 0 {
+		return errDuplicateEmail
+	}
+
+	googleIdQuery := datastore.NewQuery("User").Filter("GoogleId =", u.GoogleId).Transaction(tx)
+	_, err = datastoreClient.GetAll(*ctx, googleIdQuery, &users)
+	if err != nil {
+		return err
+	}
+	if len(users) > 0 {
+		return errDuplicateGoogleId
+	}
+
+	userKey := datastore.IncompleteKey("User", nil)
+	key, err := datastoreClient.Put(*ctx, userKey, u)
+	if err != nil {
+		return err
+	}
+	u.Id = key
+
+	return nil
+}
+
+func (u *User) Dto() *UserDto {
+	return &UserDto{
+		Name:     u.Name,
+		Username: u.Username,
+		Picture:  u.Picture,
+	}
+}
+
+type CredentialsDto struct {
+	Token    string `json:"token"`
+	Provider string `json:"provider"`
+}
+
+func sendErrorMsg(msg string, e string, w *http.ResponseWriter) {
+	sendMsg(
+		fmt.Sprintf("{\"code\": 500, \"message\": \"%v\", \"errors\": \"%v\"}", msg, e),
+		http.StatusInternalServerError,
+		w,
+	)
+}
+
+func sendUnauthorizedMsg(msg string, e string, w *http.ResponseWriter) {
+	sendMsg(
+		fmt.Sprintf("{\"code\": 401, \"message\": \"%v\", \"errors\": \"%v\"}", msg, e),
+		http.StatusUnauthorized,
+		w,
+	)
+}
+
+func sendUnprocessableEntityMsg(e string, w *http.ResponseWriter) {
+	sendMsg(
+		fmt.Sprintf("{\"code\": 422, \"message\": \"%v\", \"errors\": \"%v\"}", "Failed to process request body", e),
+		http.StatusUnprocessableEntity,
+		w,
+	)
+}
+
+func sendMethodNotAllowedMsg(e string, w *http.ResponseWriter) {
+	sendMsg(
+		fmt.Sprintf("{\"code\": 405, \"message\": \"%v\", \"errors\": \"%v\"}", "HTTP Method not allowed", e),
+		http.StatusMethodNotAllowed,
+		w,
+	)
+}
+
+func sendMsg(payload string, status int, w *http.ResponseWriter) {
+	(*w).WriteHeader(status)
+	(*w).Header().Set("Content-Type", "application/json; charset=utf-8")
+	(*w).Write([]byte(payload))
+}
+
+func ensurePostMethod(r *http.Request) error {
+	if r.Method != http.MethodPost {
+		return fmt.Errorf("method %s is not allowed", r.Method)
+	}
+	return nil
+}
+
+func parseRequestBody(r *http.Request) (*CredentialsDto, error) {
+	var credentialDto CredentialsDto
+	if err := json.NewDecoder(r.Body).Decode(&credentialDto); err != nil {
+		return nil, err
+	}
+	return &credentialDto, nil
+}
+
+func validateGoogleToken(ctx *context.Context, token string) (*idtoken.Payload, error) {
+	serverID := strings.TrimSpace(os.Getenv("SERVER_ID"))
+	googleClientID := strings.TrimSpace(os.Getenv("GOOGLE_CLIENT_ID"))
+
+	payload, err := idtoken.Validate(*ctx, token, serverID)
+	if err != nil {
+		return nil, err
+	}
+
+	claims := payload.Claims
+	azp := fmt.Sprint(claims["azp"])
+	if azp != googleClientID {
+		return nil, fmt.Errorf("client applications is not authorized by our server")
+	}
+
+	return payload, nil
+}
+
+func createNewUserFromGoogleClaims(ctx *context.Context, u *User, claims map[string]interface{}) error {
+	u.Email = fmt.Sprint(claims["email"])
+	u.GoogleId = fmt.Sprint(claims["sub"])
+	u.Name = fmt.Sprint(claims["name"])
+	u.Username = strings.Replace(uuid.New().String(), "-", "", -1)
+	if err := u.Create(ctx); err != nil {
+		return err
+	}
+	return nil
+}
+
+func SignIn(w http.ResponseWriter, r *http.Request) {
+	projectID = strings.TrimSpace(os.Getenv("GOOGLE_CLOUD_PROJECT"))
+
+	err := ensurePostMethod(r)
+	if err != nil {
+		sendMethodNotAllowedMsg(err.Error(), &w)
+		return
+	}
+
+	credentialDto, err := parseRequestBody(r)
+	if err != nil {
+		sendUnprocessableEntityMsg(err.Error(), &w)
+		return
+	}
+
+	provider := strings.ToLower(credentialDto.Provider)
+	switch provider {
+	case "google":
+		ctx := context.Background()
+		tokenPayload, err := validateGoogleToken(&ctx, credentialDto.Token)
+		if err != nil {
+			sendUnauthorizedMsg("Token rejected.", err.Error(), &w)
+			return
+		}
+
+		claims := tokenPayload.Claims
+
+		// Connect to datastore
+		datastoreClient, err = datastore.NewClient(ctx, projectID)
+		if err != nil {
+			sendErrorMsg("Failed to connect to database.", err.Error(), &w)
+		}
+		defer datastoreClient.Close()
+
+		var user User
+		if err := user.GetByGID(&ctx, fmt.Sprint(claims["sub"])); err == errNotFound {
+			err = createNewUserFromGoogleClaims(&ctx, &user, claims)
+			if err != nil {
+				sendErrorMsg("Error creating user", err.Error(), &w)
+				return
+			}
+		} else if err != nil {
+			sendErrorMsg("Failed to fetch user info.", err.Error(), &w)
+			return
+		}
+
+		fmt.Fprintf(w, "%v\n%v", user.Name, user.Username)
+
+	default:
+		sendErrorMsg("The provider is not registered in our system.", "", &w)
+		return
+	}
+
+	// projectID := os.Getenv("GOOGLE_CLOUD_PROJECT")
+	// secretClient, err := secretmanager.NewClient(ctx)
+	// if err != nil {
+	// 	sendErrorMsg("Failed to setup secret client.", err.Error(), w)
+	// 	return
+	// }
+	// defer secretClient.Close()
+
+	// secretReq := &secretmanagerpb.AccessSecretVersionRequest{
+	// 	Name: fmt.Sprintf("projects/%s/secrets/%s/versions/latest", projectID, "jwt-test"),
+	// }
+	// secretResp, err := secretClient.AccessSecretVersion(ctx, secretReq)
+	// if err != nil {
+	// 	sendErrorMsg("Failed to get jwt secret.", err.Error(), w)
+	// 	return
+	// }
+
+	// w.WriteHeader(http.StatusCreated)
+	// fmt.Fprintln(w, string(secretResp.Payload.Data))
+}
